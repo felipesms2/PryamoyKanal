895e5c17c3d7b2c2063eff1f910b68debbc6a135
felipesms2 <fmbh.1989@gmail.com>
2025-09-20 17:22:39 -0300

    scrapes

---
+++ app/Http/Controllers/ScrapeContentStatusController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreScrapeContentStatusRequest;
+use App\Http\Requests\UpdateScrapeContentStatusRequest;
+use App\Models\ScrapeContentStatus;
+
+class ScrapeContentStatusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreScrapeContentStatusRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(ScrapeContentStatus $scrapeContentStatus)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(ScrapeContentStatus $scrapeContentStatus)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateScrapeContentStatusRequest $request, ScrapeContentStatus $scrapeContentStatus)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(ScrapeContentStatus $scrapeContentStatus)
+    {
+        //
+    }
+}
---
+++ app/Models/ScrapeContentStatus.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ScrapeContentStatus extends Model
+{
+    /** @use HasFactory<\Database\Factories\ScrapeContentStatusFactory> */
+    use HasFactory;
+}
