b61b027dd987f4529d76f73f6f913c0fd77e3964
felipesms2 <fmbh.1989@gmail.com>
2025-09-20 13:34:24 -0300

    make model

---
+++ app/Http/Controllers/YoutubeController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreYoutubeRequest;
+use App\Http\Requests\UpdateYoutubeRequest;
+use App\Models\Youtube;
+
+class YoutubeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreYoutubeRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Youtube $youtube)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Youtube $youtube)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateYoutubeRequest $request, Youtube $youtube)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Youtube $youtube)
+    {
+        //
+    }
+}
